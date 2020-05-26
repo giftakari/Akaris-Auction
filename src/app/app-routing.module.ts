c2bdfd2d2549a0eebeef06ac48d59d8dc63d7876
Godsgift ThankGod Akari <giftakari@yahoo.com>
2020-05-26 14:58:40 +1000

    configured home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: 'home', component: AppComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
