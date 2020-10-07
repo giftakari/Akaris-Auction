94fe55d6563f2f981f43f482e65bd36fce2536db
Godsgift ThankGod Akari <giftakari@yahoo.com>
2020-10-08 05:42:01 +1100

    product details components

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent implements OnInit {
+  productId: string;
+  sellerId: 5678;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(route: ActivatedRoute) {
+    this.productId = route.snapshot.paramMap.get('id');
   }
 
+  ngOnInit(): void {}
 }
